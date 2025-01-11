507c972204aeed2adb3afa0d97e391d61b33a0cf
farxana jafary <farzanajafari17@gmail.com>
2025-01-11 23:34:08 +0330

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:flash_chat_starting_project/screens/chat_screen.dart';
+import 'package:flash_chat_starting_project/screens/login_screen.dart';
+import 'package:flash_chat_starting_project/screens/registration_screen.dart';
+
 import 'constants.dart';
 import 'package:flutter/material.dart';
 import '/screens/welcome_screen.dart';
@@ -5,10 +9,8 @@ import '/screens/welcome_screen.dart';
 void main() {
   runApp(FlashChat());
 }
-
 class FlashChat extends StatelessWidget {
   FlashChat({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,6 +18,13 @@ class FlashChat extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kBackgroundColor,
       ),
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id:(context) => LoginScreen(),
+        RegistrationScreen.id:(context) => RegistrationScreen(),
+        ChatScreen.id:(context) =>  ChatScreen(),
+      },
+      initialRoute: WelcomeScreen.id,
       home: WelcomeScreen(),
     );
   }
