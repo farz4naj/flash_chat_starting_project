8ea237ff6d9df7dc51f05ab0191849940092706e
farxana jafary <farzanajafari17@gmail.com>
2025-02-07 00:28:53 +0330

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_starting_project/screens/chat_screen.dart';
 import 'package:flash_chat_starting_project/screens/login_screen.dart';
 import 'package:flash_chat_starting_project/screens/registration_screen.dart';
+import 'package:flash_chat_starting_project/services/auth_serice.dart';
 import 'firebase_options.dart';
 import 'constants.dart';
 import 'package:flutter/material.dart';
@@ -12,25 +14,33 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  print('user ${AuthService().getCurrentUser}');
+
   runApp(FlashChat());
 }
 class FlashChat extends StatelessWidget {
   FlashChat({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kBackgroundColor,
-      ),
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id:(context) => LoginScreen(),
-        RegistrationScreen.id:(context) => RegistrationScreen(),
-        ChatScreen.id:(context) =>  ChatScreen(),
-      },
-      initialRoute: WelcomeScreen.id,
-      home: WelcomeScreen(),
+    return StreamBuilder<User?>(
+      stream: AuthService().authStateChanges,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kBackgroundColor,
+          ),
+          routes: {
+            WelcomeScreen.id: (context) => WelcomeScreen(),
+            LoginScreen.id:(context) => LoginScreen(),
+            RegistrationScreen.id:(context) => RegistrationScreen(),
+            ChatScreen.id:(context) =>  ChatScreen(),
+          },
+          initialRoute: AuthService().getCurrentUser !=null
+              ? ChatScreen.id
+              : WelcomeScreen.id,
+        );
+      }
     );
   }
 }
